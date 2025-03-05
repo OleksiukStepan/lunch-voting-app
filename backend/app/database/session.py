595429c0a3f890c01a5e0a5b764f4c22b640c239
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-03-05 22:08:33 +0200

    update session

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
-DATABASE_URL = "postgresql://stepan:stepan_password@lunch_db:5432/lunch"
+DATABASE_URL = "postgresql://stepan:stepan_password@db:5432/lunch"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
