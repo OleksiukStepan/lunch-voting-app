c2b4a9c3e34dc2a27bf180a056ac7831271b8f96
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-03-06 00:56:23 +0200

    fill in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from backend.app.routes import restaurant_router
+from backend.app.routes import menu_router
+from backend.app.routes import user_router
+from backend.app.routes import vote_router
+
+app = FastAPI(title="Lunch voting")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+app.include_router(restaurant_router, prefix="/restaurants", tags=["Restaurants"])
+app.include_router(menu_router, prefix="/menus", tags=["Menus"])
+app.include_router(user_router, prefix="/users", tags=["Users"])
+app.include_router(vote_router, prefix="/votes", tags=["Votes"])
