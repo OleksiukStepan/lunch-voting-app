4dc3b5ae07157447622880692f8c43733e3b7f13
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-03-06 19:31:15 +0200

    update main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
 
+from backend.app.config.settings import Settings
 from backend.app.routes import restaurant_router
 from backend.app.routes import menu_router
 from backend.app.routes import user_router
 from backend.app.routes import vote_router
+from backend.app.routes import auth_router
 
 app = FastAPI(title="Lunch voting")
 
+print(Settings().BASE_DIR)
+print(Settings().PATH_TO_DB)
 
-# app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 app.include_router(restaurant_router, prefix="/restaurants", tags=["Restaurants"])
 app.include_router(menu_router, prefix="/menus", tags=["Menus"])
 app.include_router(user_router, prefix="/users", tags=["Users"])
