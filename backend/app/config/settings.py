fc9be5df3a3977aa201831dee1376f7a19571e5b
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-03-07 19:25:12 +0200

    delete settings

--- backend/app/config/settings.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from pydantic_settings import BaseSettings
-
-
-class Settings(BaseSettings):
-    ROOT_DIR: Path = Path(__file__).resolve().parents[3]
-    BASE_DIR: Path = Path(__file__).parent.parent
-    PATH_TO_DB: str = str(BASE_DIR / "database" / "lunch.db")
