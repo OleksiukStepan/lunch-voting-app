58ba4f9cb357acde79c1b8b901ce8a647038b4b3
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-03-07 13:29:07 +0200

    add doc strings to session.py

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -1,8 +1,13 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+import os
 
-DATABASE_URL = "postgresql://stepan:stepan_password@db:5432/lunch"
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session, declarative_base
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
@@ -10,6 +15,7 @@ SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
 def get_db() -> Session:
+    """Provides a database session and ensures it is closed after use"""
     db = SessionLocal()
     try:
         yield db
